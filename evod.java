0837519d27c29792eebfdb09d965528c6eac3fa3
Pournami-Prasad27 <117921519+Pournami-Prasad27@users.noreply.github.com>
2023-03-13 15:23:53 +0530

    Add files via upload

---
+++ evod.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class evod
+{
+public static void main(String[] args)
+{
+Scanner s= new Scanner(System.in);
+System.out.println("Enter the number");
+
+int a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("The number is even");
+}
+else
+{
+System.out.println("THe number is odd");
+}
+}
+}
+
