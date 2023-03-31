16d5b2b9d49b58ea7c544e1acdf28734677a969f
Pournami-Prasad27 <117921519+Pournami-Prasad27@users.noreply.github.com>
2023-03-31 14:58:41 +0530

    Add files via upload

--- Sym.java
+++ Sym.java
@@ -1,138 +1,61 @@
 
-import java.util.*;
- 
-// Class
-public class Sym {
-    // Matrix 1
- 
-    // Method to check whether the matrix is
-    // symmetric or asymmetric
-    static void Symmetric(int mat[][], int row,
-                               int col)
-    {
-        int i, j, flag = 1;
- 
-        // Display message
-        System.out.println("The matrix formed is:");
- 
-        // Nested for loop for matrix iteration
- 
-        // Outer loop for rows
-        for (i = 0; i < row; i++) {
-            // Inner loop for columns
-            for (j = 0; j < col; j++) {
-                // Print matrix
-                System.out.print(mat[i][j] + "\t");
-            }
- 
-            System.out.println("");
-        }
- 
-        // Matrix 2
- 
-        // Finding transpose of the matrix
-        int[][] transpose = new int[row][col];
- 
-        // Again, nested for loop for matrix iteration
- 
-        // Outer loop for rows
-        for (i = 0; i < row; i++) {
- 
-            // Inner loop for columns
-            for (j = 0; j < col; j++) {
- 
-                // Print matrix elements
-                transpose[j][i] = mat[i][j];
-            }
-        }
- 
-        // Condition check over Matrix 1 with Matrix 2
- 
-        if (row == col) {
- 
-            // Outer loop for rows
-            for (i = 0; i < row; i++) {
- 
-                // Inner loop for columns
-                for (j = 0; j < col; j++) {
- 
-                    // Comparing two matrices
-                    if (mat[i][j] != transpose[i][j]) {
-                        flag = 0;
-                        break;
-                    }
-                }
- 
-                // Setting a flag value for symmetric matrix
-                if (flag == 0) {
- 
-                    // Display message
-                    System.out.print(
-                        "\nThe matrix is not symmetric");
-                    break;
-                }
-            }
- 
-            // Setting a flag value different from above
-            // for symmetric matrix
-            if (flag == 1) {
- 
-                // Display message
-                System.out.print(
-                    "\nThe matrix is symmetric");
-            }
-        }
- 
-        // If it isn't a square matrix
-        // then it can't be a symmetric matrix
-        else {
- 
-            // Display message
-            System.out.print(
-                "\nThe matrix is not symmetric");
-        }
-    }
- 
-    // Main driver method
-    public static void main(String args[])
-    {
-        // Taking input from the user
-        Scanner sc = new Scanner(System.in);
- 
-        // Declaring variables and setting flag to 1
-        int i, j, row, col, flag = 1;
- 
-        // Taking input from the user
-        System.out.print("Enter the number of rows and colums:");
-        row = sc.nextInt();
-        col = sc.nextInt();
-        // Display message
-        
-        // Reading matrix elements individually using
-        // nextInt() method
-        
- 
-        // Declaring a 2D array(matrix)
-        int[][] mat = new int[row][col];
- 
-        // Display message
-        System.out.println("Enter the matrix elements:");
- 
-        // Nested for loop for traversing matrix
- 
-        // Outer loop for rows
-        for (i = 0; i < row; i++) {
- 
-            // Inner loop for columns
-            for (j = 0; j < col; j++) {
- 
-                // Print matrix element
-                mat[i][j] = sc.nextInt();
-            }
-        }
- 
-        // calling function made above to check
-        // whether matrix is symmetric or not
-        Symmetric(mat, row, col);
-    }
+import java.util.Scanner;
+public class symmetric
+{
+public static void main(String[] args )
+{
+Scanner sc= new Scanner( System.in);
+System.out.println( "Enter the number of rows :");
+int rows = sc.nextInt();
+System.out.println("Enter the number of columns :");
+int cols = sc.nextInt();
+int matrix[][] = new int[rows][cols];
+System.out.println("Enter the elements :");
+for ( int i = 0; i < rows; i++ )
+{
+for ( int j = 0; j < cols; j++ )
+{
+matrix[i][j] = sc.nextInt();
+}
+}
+System.out.println("The input matrix is :");
+for ( int i = 0; i < rows; i++ )
+{
+for ( int j = 0; j < cols; j++ )
+{
+System.out.print(matrix[i][j]+"\t");
+}
+System.out.println();
+}
+
+
+if ( rows != cols )
+{
+System.out.println("The given matrix is not a square matrix, so it can't be symmetric.");
+}
+else
+{
+boolean symmetric = true;
+for ( int i = 0; i < rows; i++ )
+{
+for ( int j = 0; j < cols; j++ )
+{
+if ( matrix[i][j] != matrix[j][i] )
+{
+symmetric = false;
+break;
+}
+}
+}
+if ( symmetric)
+{
+System.out.println("The given matrix is symmetric.");
+}
+else
+{
+System.out.println("The given matrix is not symmetric.");
+}
+}
+sc.close();
+}
 }
